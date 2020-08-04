a25eee6b7482b2a5fcd3100a554e41a5c3d18dff
Daniel Turner <danielturner184@gmail.com>
2020-08-04 03:31:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introdution (name)
   puts "Hi, my name is #{name}"
   
 end 
-name = "Edwin"
+name = "Josh "
 language = "Python"
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
